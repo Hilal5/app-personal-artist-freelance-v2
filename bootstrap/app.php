1e4fd4c7d7f683959a5b84a9e189086cb0d1cf68
Hilal5 <hilalprayogi42@gmail.com>
2026-03-08 20:27:29 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(\App\Http\Middleware\RememberUser::class);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use App\Http\Controllers\MessageController;
 
 Route::get('/', fn() => redirect()->route('artist.profile'));
 
-Route::post('/login',  [AuthController::class, 'login'])->name('login.post');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout.post');
 
